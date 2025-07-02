396c4eeaeae6e5a2b0d6f0760b70df39e27a0587
faishal ali <faishal3900>
2025-07-02 23:34:30 +0530

    Fix: background img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 const Header = () => {
+
+
+  const backgroundStyle = {
+    backgroundImage: `url(${assets.header_img})`,
+  }
+
   return (
-    <div className='header'>
+    <div className='header' style={backgroundStyle}>
       <div className='header-contents'>
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes. crafted with the finest ingredients and culinary expertise. our mission is to stisty your cravings and elevate your dining experience, one delicious meal at a time.</p>
