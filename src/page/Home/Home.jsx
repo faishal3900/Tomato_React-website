0ae9ed15c6d8d70e2ef8eace4079a0664b563863
faishal ali <faishal3900>
2025-07-02 23:17:03 +0530

    Fix: Correct import path casing for ExploreMenu

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
-import ExploreMenu from "../../components/exploremenu/Exploremenu"
+import ExploreMenu from "../../components/ExploreMenu/EMenu"
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
